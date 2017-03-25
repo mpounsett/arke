011bdee9714efc52e0335bebdb7d36840deb51ec
Matthew Pounsett <matt@conundrum.com>
2017-03-25 17:12:41 -0500

    initial draft of tokenizer

---
+++ arke/tokenizer.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# Copyright 2017, Matthew Pounsett <matt@conundrum.com>
+# ------------------------------------------------------------
+
+import enum
+
+
+class TokenizerError(Exception):
+    pass
+
+
+class UnexpectedEOL(TokenizerError):
+    pass
+
+
+class UnexpectedEOF(TokenizerError):
+    pass
+
+
+class UnbalancedParentheses(TokenizerError):
+    pass
+
+
+class TokenType(enum.Enum):
+    EOL = 1
+    EOF = 2
+    SPACE = 3
+    COMMENT = 4
+    STRING = 5
+    WORD = 6
+
+
+DELIMITERS = [
+    ';',
+    '(',
+    ')',
+    '\n',
+    '\t',
+    ' ',
+    '"',
+    "'",
+]
+
+
+class Tokenizer(object):
+    def __init__(self, f):
+        self.f = f
+
+        # set some basic state
+        self.multiline = 0
+        self.escaped = False
+        # "start of line"
+        self.SOL = True
+
+    def __iter__(self):
+        while True:
+            token = None
+            tokentype = None
+
+            c = self._peek_next()
+
+            if self._is_whitespace(c) and self.SOL:
+                token = self._eat_whitespace()
+                tokentype = TokenType.SPACE
+                self.SOL = False
+            elif self._is_whitespace(c):
+                self._eat_whitespace()
+                self.SOL = False
+            elif c == '\n' and not self.multiline:
+                self.f.read(1)
+                tokentype = TokenType.EOL
+                self.SOL = True
+            elif c == '"':
+                # Starting a quoted string
+                token = self._get_string()
+                tokentype = TokenType.STRING
+                self.SOL = False
+            elif c == ';':
+                # Starting a comment
+                token = self._get_comment()
+                tokentype = TokenType.COMMENT
+                self.SOL = False
+            elif c == '(':
+                # starting something multiline
+                self.f.read(1)
+                self.multiline += 1
+                self.SOL = False
+            elif c == ')':
+                # ending something multiline
+                if self.multiline <= 0:
+                    raise UnbalancedParentheses
+                else:
+                    self.f.read(1)
+                    self.multiline -= 1
+                    self.SOL = False
+            elif c == '':
+                # reached the end of the file.  Check what our state is and
+                # see if anything is amiss:
+                if self.multiline:
+                    raise UnexpectedEOF(
+                        "EOF while in multiline mode ({})".format(
+                            self.multiline)
+                    )
+                else:
+                    tokentype = TokenType.EOF
+            else:
+                token = self._get_word()
+                tokentype = TokenType.WORD
+                self.SOL = False
+
+            if tokentype:
+                yield (tokentype, token)
+                if tokentype == TokenType.EOF:
+                    break
+
+    def _peek_next(self):
+        x = self.f.tell()
+        c = self.f.read(1)
+        self.f.seek(x, 0)
+        return c
+
+    def _is_whitespace(self, c):
+        if self.multiline and c == '\n':
+            return True
+        elif c == '\n':
+            return False
+        elif c.isspace():
+            return True
+
+    def _eat_whitespace(self):
+        eaten = 0
+        while 1:
+            c = self._peek_next()
+            if self._is_whitespace(c):
+                self.f.read(1)
+                eaten += 1
+            else:
+                return eaten
+
+    def _get_string(self):
+        value = ""
+        # eat the opening quote char
+        c = self.f.read(1)
+        while 1:
+            if self._peek_next() == '"' and self.escaped is False:
+                # eat the closing quote
+                self.f.read(1)
+                return value
+            c = self.f.read(1)
+            if c == '\\' and self.escaped is False:
+                self.escaped = True
+            elif self.escaped is True:
+                self.escaped = False
+            elif c == '\n':
+                raise UnexpectedEOL
+            value += c
+
+    def _get_comment(self):
+        value = ""
+        # eat the opening semicolon and whitespace
+        self.f.read(1)
+        self._eat_whitespace()
+        while True:
+            if self._peek_next() == ')' and self.multiline:
+                # while in multiline mode, a ) is not part of a comment
+                return value
+            elif self._peek_next() == "\n":
+                self._eat_whitespace()
+                return value
+            value += self.f.read(1)
+
+    def _get_word(self):
+        value = ""
+        while 1:
+            c = self._peek_next()
+            if c in DELIMITERS:
+                return value
+            if c == '':
+                self.f.read(1)
+                return value
+            value += self.f.read(1)
---
+++ tests/test_tokenizer.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# Copyright 2017, Matthew Pounsett <matt@conundrum.com>
+# ------------------------------------------------------------
+import inspect
+import os
+import sys
+import unittest
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+                )
+import arke.tokenizer
+
+
+SAMPLE_TEXT = inspect.cleandoc(
+    """
+    @ IN SOA foo.example.com. hostmaster.example.com. (
+                1 ; serial
+                3600; refresh
+                3600 ; retry
+                2 ; expiry
+                300; negttl )
+        IN A 192.0.2.1
+    foo IN A 192.0.2.2
+    """
+)
+
+SAMPLE_TOKEN = [
+    (arke.tokenizer.TokenType.WORD, '@'),
+    (arke.tokenizer.TokenType.WORD, 'IN'),
+    (arke.tokenizer.TokenType.WORD, 'SOA'),
+    (arke.tokenizer.TokenType.WORD, 'foo.example.com.'),
+    (arke.tokenizer.TokenType.WORD, 'hostmaster.example.com.'),
+    (arke.tokenizer.TokenType.WORD, '1'),
+    (arke.tokenizer.TokenType.COMMENT, 'serial'),
+    (arke.tokenizer.TokenType.WORD, '3600'),
+    (arke.tokenizer.TokenType.COMMENT, 'refresh'),
+    (arke.tokenizer.TokenType.WORD, '3600'),
+    (arke.tokenizer.TokenType.COMMENT, 'retry'),
+    (arke.tokenizer.TokenType.WORD, '2'),
+    (arke.tokenizer.TokenType.COMMENT, 'expiry'),
+    (arke.tokenizer.TokenType.WORD, '300'),
+    (arke.tokenizer.TokenType.COMMENT, 'negttl '),
+    (arke.tokenizer.TokenType.EOL, None),
+    (arke.tokenizer.TokenType.SPACE, 4),
+    (arke.tokenizer.TokenType.WORD, 'IN'),
+    (arke.tokenizer.TokenType.WORD, 'A'),
+    (arke.tokenizer.TokenType.WORD, '192.0.2.1'),
+    (arke.tokenizer.TokenType.EOL, None),
+    (arke.tokenizer.TokenType.WORD, 'foo'),
+    (arke.tokenizer.TokenType.WORD, 'IN'),
+    (arke.tokenizer.TokenType.WORD, 'A'),
+    (arke.tokenizer.TokenType.WORD, '192.0.2.2'),
+    (arke.tokenizer.TokenType.EOF, None)
+]
+
+
+class TestTokenizer(unittest.TestCase):
+    def test_tokenizer(self):
+        t = arke.tokenizer.Tokenizer(StringIO(SAMPLE_TEXT))
+        self.assertEqual(list(iter(t)), SAMPLE_TOKEN)
